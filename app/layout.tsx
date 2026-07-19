1c24e433d34e2993535f3a78c39148cb075b510d
AJ Miller T. Perez <pjomill@gmail.com>
2026-07-20 02:52:52 +0800

    Add Google login and flexible storage management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { AuthProvider } from "./auth-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><AuthProvider>{children}</AuthProvider></body>
     </html>
   );
 }
